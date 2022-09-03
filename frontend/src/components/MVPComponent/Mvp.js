47c769f7642ae941e28c37e48d366e0534bbe6e8
unknown <aryanpandey1507@gmail.com>
2022-09-03 20:10:27 +0530

    Done with yoga

--- frontend/src/components/MVPComponent/Mvp.js
+++ frontend/src/components/MVPComponent/Mvp.js
@@ -1,107 +1,140 @@
-import React, { useState } from 'react'
-import './mvp.css'
-import Assesment from './Assesment';
-import MealPlanner from './MealPlanner';
+import React, { useState } from "react";
+import "./mvp.css";
+import Assesment from "./Assesment";
+import MealPlanner from "./MealPlanner";
 const Mvp = () => {
+  const [file, setFile] = useState();
+  const [progress, setProgress] = useState(0);
+  const [result, setResult] = useState();
+  const [quantity, setQuantitiy] = useState();
+  const [avatarPreview, setAvatarPreview] = useState("");
 
-  
-  const [file,setFile] =useState();
-const [progress,setProgress] = useState(0);
-const [result,setResult] =useState();
-const [quantity,setQuantitiy] =useState();
-const [avatarPreview , setAvatarPreview] = useState('');
+  // lang --> English Translated data
+  // language --> detected language
 
+  // const [convert,setConvert] = useState()
 
-// lang --> English Translated data
-// language --> detected language
+  const onFileChange = (e) => {
+    console.log(e.target.files[0]);
+    setFile(e.target.files[0]);
+    const type = e.target.files[0].type;
+    console.log(type);
+    if (type === "application/pdf") {
+      alert(
+        "Please Use Screenshots of PDF (Not PDF)....This Feature is Yet to be Implemented"
+      );
+    }
 
-// const [convert,setConvert] = useState()
-
-const onFileChange =(e)=>{
-  console.log(e.target.files[0])
-  setFile(e.target.files[0])
-  const type = e.target.files[0].type;
-  console.log(type)
-  if(type === "application/pdf"){
-    alert('Please Use Screenshots of PDF (Not PDF)....This Feature is Yet to be Implemented')
-  }
-
-  const reader = new FileReader();
-    console.log('yooo')
-      reader.onload=()=>{
-      
-          console.log('Hi' + reader.readyState)
-          if(reader.readyState===2){
-              
-              setAvatarPreview(reader.result);
-    
-          }
+    const reader = new FileReader();
+    console.log("yooo");
+    reader.onload = () => {
+      console.log("Hi" + reader.readyState);
+      if (reader.readyState === 2) {
+        setAvatarPreview(reader.result);
       }
-      
-      
-      reader.readAsDataURL(e.target.files[0])
-}
+    };
 
-// const registerDataChange = (e)=>{
-//       const reader = new FileReader();
-// console.log('yooo')
-//       reader.onload=()=>{
-      
-//           console.log('Hi' + reader.readyState)
-//           if(reader.readyState===2){
-              
-//               setAvatarPreview(reader.result);
-    
-//           }
-//       }
-      
-      
-//       reader.readAsDataURL(e.target.files[0])
+    reader.readAsDataURL(e.target.files[0]);
+  };
 
-  
-// }
+  // const registerDataChange = (e)=>{
+  //       const reader = new FileReader();
+  // console.log('yooo')
+  //       reader.onload=()=>{
 
+  //           console.log('Hi' + reader.readyState)
+  //           if(reader.readyState===2){
+
+  //               setAvatarPreview(reader.result);
+
+  //           }
+  //       }
+
+  //       reader.readAsDataURL(e.target.files[0])
+
+  // }
+
+  const [height , setheight]=useState();
+  const [weight , setweight]=useState();
+  const [age , setage]=useState();
 
   return (
-  <div>
-    <div className="parent3">
-    <section className="left3">  
-    <p></p>
-    <i class="fa-solid fa-arrow-down"></i>
-    <input type="file" onChange={onFileChange} accept="image/"></input>
-    <span className='mx-5'>Choose Quantitiy Please </span>
-   <select value ={quantity} onChange={e=>setQuantitiy(e.target.value)}>
-      <option value={1}>Full</option>
-      <option value={1/2}>Half</option>
-      <option value={1/4}>Quarter</option>
-    </select>
+    <div>
+      <div className="parent3">
+        <section className="left3">
+          <p></p>
+          <i class="fa-solid fa-arrow-down"></i>
+          <input type="file" onChange={onFileChange} accept="image/"></input>
+          <span className="mx-5">Choose Quantitiy Please </span>
+          <select
+            value={quantity}
+            onChange={(e) => setQuantitiy(e.target.value)}
+          >
+            <option value={1}>Full</option>
+            <option value={1 / 2}>Half</option>
+            <option value={1 / 4}>Quarter</option>
+          </select>
 
-    <button>Upload Button </button >
-    
+          <button>Upload Button </button>
+        </section>
 
-    </section>
+        <section className="middle3 card card-5">
+          <h1>Extracted Data</h1>
+          {avatarPreview === "" && (
+            <>
+              <img src={avatarPreview} alt="Avatar Preview" />
+            </>
+          )}
+        </section>
+      </div>
 
+      <h2 className="homeHeading">Lets Analyse</h2>
 
+      <Assesment data="banana" />
 
-    <section className="middle3 card card-5" >
-    <h1>Extracted Data</h1>
-    {
-      (avatarPreview=== "" && (
-        <>
-        <img src={avatarPreview} alt="Avatar Preview" /> 
-        </>
-      ))
-    } 
-    </section>
-</div>
+      <MealPlanner calorie={2000} />
 
-  <h2 className='homeHeading'>Lets Analyse</h2>
+      <h2 className="homeHeading">Nutritional ASDV</h2>
 
-  <Assesment data = 'banana'/>
+      <form
+        className="signUpForm"
+        // ref={registerTab}
+        // onSubmit={registerSubmit}
+      >
+        <div className="signUpName">
+          <input
+            type="text"
+            placeholder="Height"
+            required
+            name="height"
+            value={height}
+          />
+        </div>
 
-  <MealPlanner calorie = {2000} />
-  </div>
-  )
-}
+        <div className="signUpName">
+          <input
+            type="text"
+            placeholder="Weight"
+            required
+            name="weight"
+            value={weight}
+          />
+        </div>
 
-export default Mvp
+        <div className="signUpName">
+          <input
+            type="text"
+            placeholder="Age"
+            required
+            name="age"
+            value={age}
+          />
+        </div>
+
+      </form>
+
+    </div>
+  );
+};
+
+export default Mvp;
